495097627635cffa6990f7fd0336f8dcc7a9bb43
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-02-23 12:56:06 +0100

    jour 17 - 18: réorganisation des fichiers scss

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "../src/style/style.css"
-import "../src/style/aboutCollapses.css"
-import "../src/style/error.css"
-import "../src/style/logementPages.css"
+import "../src/style/dimensions-and-spacing.css"
+import "../src/style/merging.css"
+import "../src/style/animations.css"
 <script src="https://kit.fontawesome.com/56bc717981.js" crossorigin="anonymous"></script>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
