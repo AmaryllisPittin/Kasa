9167887a8fe0016a1a4135528d216fb27898db78
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-04-18 10:09:30 +0200

    p11 en cours: tentative de correction des animations collapseAbout

--- src/components/collapses/CollapseAbout.jsx
+++ src/components/collapses/CollapseAbout.jsx
@@ -20,13 +20,19 @@ const CollapseAbout = ({ title, text, id }) => {
       <input type="checkbox" id={id} className={`collapse__checkbox ${chevronClass} visually-hidden`} />
       <label htmlFor={id} className="collapse collapse-about">
         {title}
-        <FontAwesomeIcon icon={faChevronUp} className={`collapse__chevron ${chevronClass}`} onClick={toggleCollapse} />
+        <FontAwesomeIcon icon={faChevronUp} className={`collapse__chevron ${chevronClass}`} />
       </label>
-      <div ref={contentRef} className={`collapse-container__presentation ${isCollapsed ? '' : 'open'}`} style={{ height: isCollapsed ? '0' : `${contentRef.current.scrollHeight}px` }} >
-        <p className="collapse-container__text">{text}</p>
+      <div className="collapse-container__dimensions" onClick={(e) => e.stopPropagation()}>
+      <div ref={contentRef} className={`collapse-container__presentation ${isCollapsed ? '' : 'open'}`} style={{ height: isCollapsed ? '0' : `${contentRef.current.scrollHeight}px` }}>
+          <p className="collapse-container__text">{text}</p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default CollapseAbout;
+
+
+
+
