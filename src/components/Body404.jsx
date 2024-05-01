a3a3232d10c0ea712a18c09e04ee9358951a0330
Amaryllis Pittin <amaryllispittin@gmail.com>
2024-05-01 18:01:46 +0200

    correction d'une erreur dans la page 404

--- src/components/Body404.jsx
+++ src/components/Body404.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 
 const Body404 = () => {
   return (
-    <body className="body-error">
+    <div className="body-error">
         <h1 className="body-error__404">404</h1>
         <p className="body-error__text">Oups ! La page que vous demandez n'existe pas.</p>
         <NavLink to="/" className="body-error__link">Retourner sur la page d'accueil</NavLink>
-    </body>
+    </div>
   );
 };
 
